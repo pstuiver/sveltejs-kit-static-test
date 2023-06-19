0d4ca5da8d7fbf46e799a8f236be801263876e1d
pstuiver <pstuiverx@gmail.com>
2023-06-19 22:51:36 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<p>Just a page. Go to <a href="./about">about page</a></p>
+<h1>Home Page Logo</h1>
+<img src="favicon.png" alt="logo">
---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html' // may differ from host to host
+		})
+	}
+};
+
+export default config;

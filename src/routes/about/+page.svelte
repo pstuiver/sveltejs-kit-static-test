4123d1714e614c9ca5f1d34527e17c1af2c3e531
pstuiver <pstuiverx@gmail.com>
2023-07-08 21:37:57 +0200

    1 binaries uploaded

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,3 @@
 <p>My about page! Go <a href="/">home</a></p>
 <h1>About Page Logo</h1>
-<img src="../src/lib/images/svelte-welcome.png" alt="another logo" width="300px">
+<img src="../src/lib/images/svelte-welcome.png" alt="another logo" width="300px" />

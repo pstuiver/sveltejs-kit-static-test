15033aa36e0c01c801fb20c0024b910098e9d084
pstuiver <pstuiverx@gmail.com>
2023-07-08 21:53:06 +0200

    1 binaries uploaded

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
 export default {
-    kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
-    }
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			base: '/build',
+
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
